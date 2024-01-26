457993ac60afa8a1d00c8ada316d60fa16983b87
Carlosnoe10 <carlosdnoem@gmail.com>
2024-01-26 11:55:08 -0800

    Terminado el login

--- src/lab2progra2_carlosnoé1/LAB2Progra2_CarlosNoé1.java
+++ src/lab2progra2_carlosnoé1/LAB2Progra2_CarlosNoé1.java
@@ -5,45 +5,54 @@ import java.util.Scanner;
 public class LAB2Progra2_CarlosNoé1 {
 
     public static void main(String[] args) {
-        
-        
-        
-        
-        Scanner sc = new Scanner(System.in);
-        boolean Bandera = true;
-        while (Bandera != false) {
-            System.out.println("--Bienvenido al menu-- \n"
-                    + "Escriba '1' Si quiere Ingresar a listar recurso \n"
-                    + "Escriba '2' Si quiere Ingresar a Crear recurso \n"
-                    + "Escriba '3' Si quiere Ingresar a Eliminar Recurso \n"
-                    + "Escriba '4' Si quiere Ingresar a Modificar Recurso \n"
-                    + "Escriba '5' para Salir ");
-            int NOM = sc.nextInt();
-            if ((NOM > 0) && (NOM < 6)) {
-                switch (NOM) {
-                    case 1:
+        Scanner ONE = new Scanner(System.in);
 
-                        break;
-                    case 2:
-                        
-                        break;
-                    case 3:
+        Usuario INICIO = new Usuario("Registro", "Registro", "Estudiante");
+        System.out.println("Ingrese Usuario");
+        String Nombre = ONE.nextLine();
+        ONE = new Scanner(System.in);
+        System.out.println("Ingrese Contraseña");
+        String Contrasenya = ONE.nextLine();
 
-                        break;
-                    case 4:
+        if (INICIO.getNombreUsu().contentEquals(Nombre) && INICIO.getContrasenya().equals(Contrasenya) ) {
 
-                        break;
-                    case 5:
-                        Bandera = false;
-                        break;
+            Scanner sc = new Scanner(System.in);
+            boolean Bandera = true;
+            while (Bandera != false) {
+                System.out.println("--Bienvenido al menu-- \n"
+                        + "Escriba '1' Si quiere Ingresar a listar recurso \n"
+                        + "Escriba '2' Si quiere Ingresar a Crear recurso \n"
+                        + "Escriba '3' Si quiere Ingresar a Eliminar Recurso \n"
+                        + "Escriba '4' Si quiere Ingresar a Modificar Recurso \n"
+                        + "Escriba '5' para Salir ");
+                int NOM = sc.nextInt();
+                if ((NOM > 0) && (NOM < 6)) {
+                    switch (NOM) {
+                        case 1:
+
+                            break;
+                        case 2:
+
+                            break;
+                        case 3:
+
+                            break;
+                        case 4:
+
+                            break;
+                        case 5:
+                            Bandera = false;
+                            break;
+                    }
+
+                } else {
+                    System.out.println("Numero mal Ingresados");
+
                 }
-
-            } else {
-                System.out.println("Numero mal Ingresados");
-
             }
+        }else{
+            System.out.println("Usuario o contrasenya Invalidos");  
         }
-
     }
 
 }
